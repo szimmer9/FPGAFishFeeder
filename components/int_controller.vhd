25ab7f4f130d5da1d21a679327e69826a3f57f28
atwilla <atiwlla@vols.utk.edu>
2020-11-04 20:17:58 -0500

    Create integer incrementer/decrementer with testbench.

---
+++ components/int_controller.vhd
@@ -0,0 +1,46 @@
+/* Written by: Alex Twilla
+   Date: 4 Nov 2020
+   Description: This entity allows one to increment or decrement an integer 
+     between some min and max values. This integer is output as a 
+     std_logic_vector, the size of which is determined by the user.
+*/
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity int_controller is
+  generic (int_lower, int_upper : integer;
+           num_out_msb, num_out_lsb : integer);
+           
+  port (en, reset, inc, dec : in std_logic;
+        num_out : out std_logic_vector (num_out_msb downto num_out_lsb));
+end int_controller;
+
+architecture Behavioral of int_controller is
+  signal int : integer range int_lower to int_upper := int_lower;
+begin
+
+  process(all)
+  begin
+    
+    if rising_edge(inc) and en = '1' then
+      if int = int_upper then
+        int <= int_lower;
+      else
+        int <= int + 1;
+      end if;
+    elsif rising_edge(dec) and en = '1' then
+      if int = int_lower then
+        int <= int_upper;
+      else
+        int <= int - 1;
+      end if;
+    elsif rising_edge(reset) then
+      int <= int_lower;
+    end if;
+    
+  end process;
+  
+  num_out <= std_logic_vector(to_unsigned(int, num_out_msb - num_out_lsb + 1));
+
+end Behavioral;
---
+++ testbenches/int_controller_tb.vhd
@@ -0,0 +1,45 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity int_controller_tb is
+end int_controller_tb;
+
+architecture Behavioral of int_controller_tb is
+  signal clk : std_logic := '0';
+  signal en, inc, dec, reset : std_logic := '1';
+  signal hr_out, min_out : std_logic_vector (5 downto 0) := "000000";
+begin
+  reset <= '0';
+  
+  hr_ctrl: entity work.int_controller
+    generic map(int_lower => 1,
+                int_upper => 12,
+                num_out_msb => 5,
+                num_out_lsb => 0)
+                
+    port map(en => en,
+             inc => inc,
+             dec => dec,
+             reset => reset,
+             num_out => hr_out);   
+             
+  min_ctrl: entity work.int_controller
+    generic map(int_lower => 0,
+                int_upper => 59,
+                num_out_msb => 5,
+                num_out_lsb => 0)
+                
+    port map(en => en,
+             inc => inc,
+             dec => dec,
+             reset => reset,
+             num_out => min_out);
+             
+  process
+  begin
+    wait for 0.5 ns;
+    dec <= not dec;
+  end process;                                                                
+
+end Behavioral;
