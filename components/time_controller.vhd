62d0034452e4d2e91459380c8db3ee9189c5aafa
Alex Twilla <alexandertwilla@gmail.com>
2020-11-11 09:07:09 -0500

    Change component to have seperate hr and min inputs.

--- components/time_controller.vhd
+++ components/time_controller.vhd
@@ -9,22 +9,13 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity time_controller is
- port (inc, dec, hr_min_sel, en, reset : in std_logic;
+ port (hr_inc, hr_dec, min_inc, min_dec, en, reset : in std_logic;
        hr, min : out std_logic_vector (5 downto 0));
 end time_controller;
 
 architecture Behavioral of time_controller is
-  signal hr_inc, hr_dec, hr_en : std_logic;
-  signal min_inc, min_dec, min_en : std_logic;
-begin
 
-  hr_inc <= inc and not hr_min_sel;
-  hr_dec <= dec and not hr_min_sel;
-  hr_en <= en and not hr_min_sel;
-  
-  min_inc <= inc and hr_min_sel;
-  min_dec <= dec and hr_min_sel;
-  min_en <= en and hr_min_sel;
+begin
 
   hr_controller: entity work.int_controller
     generic map(int_lower => 1,
@@ -34,7 +25,7 @@ begin
                 
     port map(inc => hr_inc,
              dec => hr_dec,
-             en => hr_en,
+             en => en,
              reset => reset,
              num_out => hr);
              
@@ -46,7 +37,7 @@ begin
                 
     port map(inc => min_inc,
              dec => min_dec,
-             en => min_en,
+             en => en,
              reset => reset,
              num_out => min);                                           
 
