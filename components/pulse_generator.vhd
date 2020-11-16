af6f51e6aa18636db7c6ca2c0a2d991fec321a11
Alex Twilla <alexandertwilla@gmail.com>
2020-11-16 14:01:15 -0500

    Rework module to function as an FSM.

--- components/pulse_generator.vhd
+++ components/pulse_generator.vhd
@@ -19,35 +19,60 @@ end pulse_generator;
 
 architecture Behavioral of pulse_generator is
 
-  signal active : std_logic := '0';
+  type fsm_state is (waiting, counting);
+  signal curr_state, next_state : fsm_state := waiting;
+  signal p1, p2, p3, r_edge_p : std_logic := '0';
   signal cycles : integer range 0 to len := 0;
 
 begin
-
-  process(pulse_in, clk)
-  begin
-    if rising_edge(pulse_in) then
-      active <= '1';
-    elsif cycles = 0 and falling_edge(clk) then
-      -- Falling edge to ensure count updates first.
-      active <= '0';
-    end if;
-  end process;
   
-  process(active, clk)
+  detect_edge: process(clk)
   begin
-    if active = '1' and rising_edge(clk) then
+    if rising_edge(clk) then
+      p1 <= pulse_in;
+      p2 <= p1;
+      p3 <= p2;
+      r_edge_p <= p2 and not p3;
+    end if;
+  end process detect_edge;
+  
+  next_state_logic: process(clk)
+  begin
+    if rising_edge(clk) then
+      if curr_state = waiting then
+        if r_edge_p then
+          next_state <= counting;
+        end if;
       
-      if cycles = len - 1 then
-        cycles <= 0;
-      else
+      elsif curr_state = counting then
+        if cycles = len - 1 then
+          next_state <= waiting;
+        end if;
+      end if;
+    end if;
+  end process next_state_logic;
+  
+  update_state: process(next_state, clk)
+  begin
+    if rising_edge(clk) then
+      curr_state <= next_state;
+    end if;
+  end process update_state;
+  
+  output_function: process(clk)
+  begin
+    if rising_edge(clk) then
+      if curr_state = counting then
         cycles <= cycles + 1;
         pulse_out <= '1';
+      elsif curr_state = waiting then
+        cycles <= 0;
+        pulse_out <= '0';
+      else
+        cycles <= 0;
+        pulse_out <= '0';
       end if;
-    elsif active = '0' then
-      pulse_out <= '0';
     end if;
-  end process;
-  
+  end process output_function;
 
 end Behavioral;
