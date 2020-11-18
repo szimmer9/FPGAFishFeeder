397fe2156addb674d77c5d689d81538fef77199d
Alex Twilla <alexandertwilla@gmail.com>
2020-11-18 15:01:15 -0500

    Change testbench to test updated time_controller.

--- testbenches/time_controller_tb.vhd
+++ testbenches/time_controller_tb.vhd
@@ -5,31 +5,35 @@ entity time_controller_tb is
 end time_controller_tb;
 
 architecture Behavioral of time_controller_tb is
-  signal inc, dec, hr_min_sel, en, reset : std_logic := '0';
+  signal hr_inc, hr_dec, min_inc, min_dec, pm_in, pm_out, en, reset, clk : std_logic := '0';
   signal hr, min : std_logic_vector(5 downto 0);
 begin
 
   en <= '1';
   
   time_ctrl: entity work.time_controller
-    port map(inc => inc,
-             dec => dec,
-             hr_min_sel => hr_min_sel,
+    port map(hr_inc => hr_inc,
+             hr_dec => hr_dec,
+             min_inc => min_inc,
+             min_dec => min_dec,
+             pm_in => pm_in,
              en => en,
              reset => reset,
              hr => hr,
-             min => min);
+             min => min,
+             clk => clk);
   
   process
   begin
-    wait for 0.5 ns;
-    dec <= not dec;
+    wait for 1 ns;
+    hr_dec <= not hr_dec;
+    min_inc <= not min_inc;
   end process;
   
   process
   begin
-    wait for 100 ns;
-    hr_min_sel <= not hr_min_sel;
+    wait for 0.5 ns;
+    clk <= not clk;
   end process;
   
   process
